899f876a4ef12c5cbabe9772d28d028cdee1908d
lotexiu <68831438+lotexiu@users.noreply.github.com>
2022-04-14 14:31:07 -0300

    fixed just only note_on

--- MidiPlayer/midi player.py
+++ MidiPlayer/midi player.py
@@ -3,16 +3,13 @@ import keyboard
 import mido
 import os
 
-# 62 = 38 (D)
-# != = -24
-#os.system('start chrome.exe https://virtualpiano.net/')
-
 keys = ['1', 'shift + 1', '2', 'shift + 2', '3', '4', 'shift + 4', '5', 'shift + 5', '6', 'shift + 6',
         '7', '8', 'shift + 8', '9', 'shift + 9', '0', 'q', 'shift + q', 'w', 'shift + w', 'e', 'shift + e',
         'r', 't', 'shift + t', 'y', 'shift + y', 'u', 'i', 'shift + i', 'o', 'shift + o', 'p', 'shift + p',
         'a', 's', 'shift + s', 'd', 'shift + d', 'f', 'g', 'shift + g', 'h', 'shift + h', 'j', 'shift + j',
         'k', 'l', 'shift + l', 'z', 'shift + z', 'x', 'c', 'shift + c', 'v', 'shift + v', 'b', 'shift + b',
         'n', 'm']
+
 
 while True:
     while True:
@@ -67,25 +64,71 @@ while True:
             break
         time.sleep(0.1)
 
+    count_on = 0
+    count_off = 0
 
-    for msg in mid.play():
+    for msg in mid:
         msg = str(msg)
         if 'note_on' in msg:
-            valor = msg[23:26]
-            y = ''
-            for fix in valor:
-                if fix in str((1, 2, 3, 4, 5, 6, 7, 8, 9, 0)):
-                    y += fix
+            count_on += 1
+        elif 'note_off' in msg:
+            count_off += 1
 
-            x = int(y) - note
-            if x > max:
-                x -= (higher - max)
+    if count_on != count_off: # if have only ON
+        for msg in mid:
+            msg = str(msg)
+            if 'note_on' in msg:
+                valor = msg[23:26]
+                y = ''
+                for fix in valor:
+                    if fix in str((1, 2, 3, 4, 5, 6, 7, 8, 9, 0)):
+                        y += fix
+                x = int(y) - 24
+                exec(f'note{x} = 0')
 
-            keyboard.release(keys[x])
-            keyboard.press(keys[x])
+        for msg in mid.play():
+            msg = str(msg)
+            if 'note_on' in msg:
+                valor = msg[23:26]
+                y = ''
+                for fix in valor:
+                    if fix in str((1, 2, 3, 4, 5, 6, 7, 8, 9, 0)):
+                        y += fix
+                x = int(y) - 24
 
-        if keyboard.is_pressed('del'):
-            keyboard.release('del')
-            for soltar in keys:
-                keyboard.release(soltar)
-            break
+                if eval(f'note{x}') == 0: #press
+                    keyboard.press(keys[x])
+                    exec(f'note{x} = 1')
+
+                elif eval(f'note{x}') == 1: #release
+                    keyboard.release(keys[x])
+                    exec(f'note{x} = 0')
+
+                if keyboard.is_pressed('del'): #stop
+                    keyboard.release('del')
+                    for soltar in keys:
+                        keyboard.release(soltar)
+                    break
+
+    else: # if have ON and OFF
+        for msg in mid.play():
+            msg = str(msg)
+            if 'note_on' in msg:
+                valor = msg[23:26]
+                y = ''
+                for fix in valor:
+                    if fix in str((1, 2, 3, 4, 5, 6, 7, 8, 9, 0)):
+                        y += fix
+
+                x = int(y) - note
+                if x > max:
+                    x -= (higher - max)
+
+                keyboard.release(keys[x])
+                keyboard.press(keys[x])
+
+            if keyboard.is_pressed('del'):
+                keyboard.release('del')
+                for soltar in keys:
+                    keyboard.release(soltar)
+                break
